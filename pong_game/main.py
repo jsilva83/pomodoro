963fd274edd5a26ba0759642345bfce939646516
jsilva83 <jorge.h.silva@gmail.com>
2021-11-15 20:36:15 +0100

    Update methods to move paddle. Refactoring of vars and class paddle and creation of a left and a right paddle.

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,28 +1,39 @@
 # Importing packages.
 import time
 import turtle
-import right_paddle
+import paddle
 
 # Constants.
 GAME_WINDOW_WIDTH = 800
 GAME_WINDOW_HEIGHT = 600
 GAME_WINDOW_BACKGROUND_COLOR = 'black'
 GAME_WINDOW_TITLE = 'My Pong Game'
+PADDLE_SPEED = 0.1
+R_PADDLE_INITIAL_POSITIONS = [(370, 20), (370, 0), (370, -20)]
+L_PADDLE_INITIAL_POSITIONS = [(-380, 20), (-380, 0), (-380, -20)]
 
 # Game initialization.
 game_window = turtle.Screen()
 game_window.setup(width=GAME_WINDOW_WIDTH, height=GAME_WINDOW_HEIGHT)
 game_window.bgcolor(GAME_WINDOW_BACKGROUND_COLOR)
 game_window.title(GAME_WINDOW_TITLE)
+# Hide the details of setting the squares in position and moving
+game_window.tracer(0)
 # Create right paddle.
-r_paddle = right_paddle.RightPaddle()
-for _ in range(1, 15):
-    r_paddle.move_up()
-    time.sleep(1)
-for _ in range(1, 30):
-    r_paddle.move_down()
-    time.sleep(1)
-
-
+r_paddle = paddle.Paddle(R_PADDLE_INITIAL_POSITIONS, game_window)
+l_paddle = paddle.Paddle(L_PADDLE_INITIAL_POSITIONS, game_window)
+# Listen to keystrokes
+game_window.listen()
+game_window.onkeypress(key='q', fun=l_paddle.move_up)
+game_window.onkeypress(key='a', fun=l_paddle.move_down)
+game_window.onkeypress(key='p', fun=r_paddle.move_up)
+game_window.onkeypress(key='l', fun=r_paddle.move_down)
+# Start the game
+game_is_on = True
+while game_is_on:
+    # Update the window with the last movements
+    game_window.update()
+    time.sleep(PADDLE_SPEED)
+    pass
 # Exit window on click.
 game_window.exitonclick()
--- pong_game/paddle.py
+++ pong_game/paddle.py
@@ -3,38 +3,38 @@ import time
 import turtle
 
 # Constants.
-R_PADDLE_INITIAL_POSITIONS = [(350, 20), (350, 0), (350, -20)]
-R_PADDLE_SHAPE = 'square'
-R_PADDLE_COLOR = 'white'
-R_PADDLE_UP = 90
-R_PADDLE_DOWN = -90
-R_PADDLE_DISTANCE_MOVE = 20
-WINDOW_MAX_HEIGHT_POSITION = 280
+PADDLE_SHAPE = 'square'
+PADDLE_COLOR = 'white'
+PADDLE_UP = 90
+PADDLE_DOWN = -90
+PADDLE_DISTANCE_MOVE = 20
+WINDOW_MAX_HEIGHT_POSITION = 300
 WINDOW_MIN_HEIGHT_POSITION = -280
 PROXIMITY_TOLERANCE = 10
 
 
-class RightPaddle(turtle.Turtle):
+class Paddle(turtle.Turtle):
     """This class extends the turtle class"""
 
-    def __init__(self) -> None:
+    def __init__(self, paddle_initial_positions, obj_window) -> None:
         super().__init__()
         self.segments = []
-        for position in R_PADDLE_INITIAL_POSITIONS:
+        for position in paddle_initial_positions:
             self.add_segment(position)
         # Sets heading of the paddle head.
         self.head = self.segments[0]
-        self.head.setheading(R_PADDLE_UP)
+        self.head.setheading(PADDLE_UP)
         # Sets heading of the paddle tail.
         self.tail = self.segments[-1]
-        self.tail.setheading(R_PADDLE_DOWN)
+        self.tail.setheading(PADDLE_DOWN)
+        self.window = obj_window
         return
 
     def add_segment(self, position) -> None:
         """Creates de 3 segments of the paddle."""
         new_segment = turtle.Turtle()
-        new_segment.shape(R_PADDLE_SHAPE)
-        new_segment.color(R_PADDLE_COLOR)
+        new_segment.shape(PADDLE_SHAPE)
+        new_segment.color(PADDLE_COLOR)
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
@@ -45,7 +45,7 @@ class RightPaddle(turtle.Turtle):
             return
         for n in range(len(self.segments) - 1, 0, -1):
             self.segments[n].goto(self.segments[n - 1].position())
-        self.head.forward(R_PADDLE_DISTANCE_MOVE)
+        self.head.forward(PADDLE_DISTANCE_MOVE)
         return
 
     def move_down(self) -> None:
@@ -54,5 +54,5 @@ class RightPaddle(turtle.Turtle):
             return
         for n in range(0, len(self.segments) - 1):
             self.segments[n].goto(self.segments[n + 1].position())
-        self.tail.forward(R_PADDLE_DISTANCE_MOVE)
+        self.tail.forward(PADDLE_DISTANCE_MOVE)
         return
